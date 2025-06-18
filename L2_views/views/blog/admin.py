038eabed70b34c71d0f00d81c43631f3c187f393
papanda <ru3aah@gmail.com>
2025-06-18 23:46:46 +0300

    end of admin presentation. Tags have been managed to be handled in admin.py

--- L2_views/views/blog/admin.py
+++ L2_views/views/blog/admin.py
@@ -1,25 +1,28 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post, Comment, Author, Tag, Category
-
 
 class InlineCommentAdmin(admin.TabularInline):
     model = Comment
     extra = 0
     fields = ('content', 'author')
 
+class TagInline(admin.TabularInline):
+    model = Tag.posts.through
+    extra = 1
+    verbose_name = "Tag"
+    verbose_name_plural = "Tags"
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'author', 'created_at',
-                    'updated')
+    list_display = ('id', 'status', 'title', 'category', 'author', 'created_at',
+                    'updated', 'display_tags')    
     list_display_links = ('id',)
-    list_editable = ('title', 'category', 'author')
+    list_editable = ('title', 'category', 'author', 'status')
     list_filter = ('status', 'author', 'category')
+    list_select_related = ('author', 'category')
     search_fields = ('title', 'content')
     actions = ['publish', 'make_draft']
-    inlines = [InlineCommentAdmin]
+    inlines = [InlineCommentAdmin, TagInline]
 
     fieldsets = (
             ('Basic data', {
@@ -35,33 +38,66 @@ class PostAdmin(admin.ModelAdmin):
                 'classes': ('collapse',)
             }),
     )
-    readonly_fields = ('created_at', 'updated_at', 'updated')
+    readonly_fields = ('created_at', 'updated_at', 'updated', 'display_tags')
 
-
-    @admin.display(description='updated')
+    @admin.display(description='Last Updated')
     def updated(self, post: Post):
-        return 'not updated' if (post.updated_at.isoformat(timespec='minutes')
-                                 ==
-                                 post.created_at.isoformat(
-                                     timespec='minutes')) else post.updated_at
+        is_updated = (post.updated_at.isoformat(timespec='minutes') !=
+                      post.created_at.isoformat(timespec='minutes'))
+        return post.updated_at if is_updated else 'not updated'
 
+    @admin.display(description='Tags')
+    def display_tags(self, post: Post):
+        if not post.tags.exists():
+            return 'No tags'    
+        return '|'.join([tag.title for tag in post.tags.all()])
 
     def publish(self, request, queryset):
-        self.message_user(request, "Post has been published")
+        count = queryset.count()
         queryset.update(status=Post.PUBLISHED)
-
+        self.message_user(request,
+                          f"{count} post{'s' if count != 1 else ''} " 
+                                   f"{'have' if count != 1 else 'has'} been "
+                                   f"published")
 
     def make_draft(self, request, queryset):
-        self.message_user(request, "Post has been unpublished")
+        count = queryset.count()
         queryset.update(status=Post.DRAFT)
+        self.message_user(request,
+                          f"{count} post{'s' if count != 1 else ''} "
+                          f"{'have' if count != 1 else 'has'} been unpublished")
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    filter_horizontal = ('posts',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
 
 
-admin.site.register(Comment)
-admin.site.register(Author)
-admin.site.register(Tag)
-admin.site.register(Category)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'author', 'post', 'created_at')
+    list_display_links = ('id', 'content')
+    search_fields = ('content',)
+    list_filter = ('author', 'post')
 
+    def get_queryset(self, request):
+        return (super().get_queryset(request).
+                select_related('author', 'post'))
 
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'joined_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
 
-
-
+#admin.site.register(Comment)
+#admin.site.register(Author)
+#admin.site.register(Category)
