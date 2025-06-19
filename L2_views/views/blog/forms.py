ce870c9561d4c26e095bec9bfb0088012ba8d34a
papanda <ru3aah@gmail.com>
2025-06-19 14:46:22 +0300

    added validators and attributes

--- L2_views/views/blog/forms.py
+++ L2_views/views/blog/forms.py
@@ -1,17 +1,27 @@
 from django import forms
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=False,
                         widget=forms.TextInput(
                                attrs={'placeholder': 'Enter your Name',
-                                      'class':'form-control'}
-                        )
+                                      'class':'form-control'
+                                      }
+                        ), validators=[MinLengthValidator(2,
+                            'Name must be greater than 2 characters'),
+                                    MaxLengthValidator(100,
+                            'Name must be less than 100 characters')
+                                       ]
     )
     email = forms.EmailField(label='E-mail',
                             widget=forms.EmailInput(
                                  attrs={'placeholder': 'your E-mail',
-                                        'class':'form-control'}
-                            )
+                                        'class':'form-control'
+                                        }
+                            ),
+                    error_messages={'invalid': 'Enter a valid email address.',
+                                    'required': 'This field is required.'
+                                    }
     )
     subject = forms.CharField(max_length=100,
                             widget=forms.TextInput(
