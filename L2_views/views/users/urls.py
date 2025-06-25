8a7e8e8f92103d941b965afc7dcee44d2254a806
papanda <ru3aah@gmail.com>
2025-06-26 01:46:04 +0300

    added user registration

--- L2_views/views/users/urls.py
+++ L2_views/views/users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
+from .views import RegisterView
 
 app_name = 'users'
 
@@ -9,6 +10,6 @@ urlpatterns = [
         template_name='users/login.html',
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', views.register, name='register')
+    path('register/', RegisterView.as_view(), name='register')
     #path('profile/', views.profile, name='profile')
 ]
--- L2_views/views/users/views.py
+++ L2_views/views/users/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -20,3 +23,8 @@ def log_in(request):
 def log_out(request):
     logout(request)
     return render(request, 'users/login.html')
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('users:login') #returns view URL by view name
