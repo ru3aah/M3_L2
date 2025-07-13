8b806b286ee4d203fc266fc2f134d5efa0b7a616
papanda <ru3aah@gmail.com>
2025-07-13 19:06:51 +0300

    api Model Serializer demo added

---
+++ L2_views/views/blog/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from ..models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content', 'created_at', 'updated_at', 'author', 'post')
+        read_only_fields = ('created_at', 'updated_at', 'author', 'post')
+
+
